d5027881e20d900662516208954ef77a04014ac8
Chamikara Vithanage <it21219498@my.sliit.lk>
2025-04-20 16:44:54 +0530

    Update API_BASE_URL to use production endpoint

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,4 +1,5 @@
-export const API_BASE_URL = "http://localhost:8000/api/";
+export const API_BASE_URL =
+  "https://gateway-965928461642.us-central1.run.app/api/";
 
 export const USER_SERVICE_BASE_URL = `${API_BASE_URL}user`;
 export const COURSE_SERVICE_BASE_URL = `${API_BASE_URL}course/course`;
