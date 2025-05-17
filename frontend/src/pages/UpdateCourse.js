dcffc2b87620fdc693c89a127ae35c09c8255b2d
Ashen_IT21286278 <99505510+IT21286278@users.noreply.github.com>
2025-05-18 02:57:55 +0530

    Refactor UpdateCourse.js for consistent string quoting and formatting

--- frontend/src/pages/UpdateCourse.js
+++ frontend/src/pages/UpdateCourse.js
@@ -1,30 +1,29 @@
-import React, { useContext, useEffect, useState } from "react";
-import CommonContext from "../context/CommonContext";
-import ToastContext from "../context/ToastContext";
-import Loading from "../components/Loading";
-import UploadWidget from "../components/UploadWidget";
-import Delete from "@mui/icons-material/Delete";
-import Tooltip from "@mui/material/Tooltip";
-import IconButton from "@mui/material/IconButton";
-import DeleteIcon from "@mui/icons-material/Delete";
-import { API_BASE_URL } from "../config/config";
+import React, { useContext, useEffect, useState } from 'react';
+import CommonContext from '../context/CommonContext';
+import ToastContext from '../context/ToastContext';
+import Loading from '../components/Loading';
+import UploadWidget from '../components/UploadWidget';
+import Delete from '@mui/icons-material/Delete';
+import Tooltip from '@mui/material/Tooltip';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { API_BASE_URL } from '../config/config';
 
 const UpdateCourse = () => {
   const { selectedCourseId } = useContext(CommonContext);
   const { toast } = useContext(ToastContext);
   const [course, setCourse] = useState(null);
-  console.log("🚀 ~ UpdateCourse ~ course:", course);
+  console.log('🚀 ~ UpdateCourse ~ course:', course);
   const [image, setImage] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
+  const [error, setError] = useState('');
 
   useEffect(() => {
     handleCourseClick();
   }, [selectedCourseId]);
 
   const handleCourseClick = async () => {
-    // localStorage.setItem("selectedCourseId", course);
-    const courseId = localStorage.getItem("selectedCourseId");
+    const courseId = localStorage.getItem('selectedCourseId');
     const data = await fetch(
       `${API_BASE_URL}course/course/getCourseById/${courseId}`
     );
@@ -53,7 +52,7 @@ const UpdateCourse = () => {
       !course.category ||
       !course.price
     ) {
-      setError("Please fill in all fields");
+      setError('Please fill in all fields');
       setLoading(false);
       return;
     }
@@ -62,9 +61,9 @@ const UpdateCourse = () => {
       const response = await fetch(
         `${API_BASE_URL}course/course/updateCourse`,
         {
-          method: "PUT",
+          method: 'PUT',
           headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
           },
           body: JSON.stringify({
             title: course.title,
@@ -84,10 +83,10 @@ const UpdateCourse = () => {
         setLoading(false);
         return;
       } else {
-        toast.success("Course updated successfully!");
+        toast.success('Course updated successfully!');
       }
     } catch (error) {
-      console.error("Error updating course", error);
+      console.error('Error updating course', error);
     }
 
     setLoading(false);
@@ -111,7 +110,7 @@ const UpdateCourse = () => {
   const handleDeleteCourse = async () => {
     //confirm box
     const confirm = window.confirm(
-      "Are you sure you want to delete this course?"
+      'Are you sure you want to delete this course?'
     );
     if (!confirm) {
       return;
@@ -120,18 +119,18 @@ const UpdateCourse = () => {
       const response = await fetch(
         `${API_BASE_URL}course/course/deleteCourse/${selectedCourseId}`,
         {
-          method: "DELETE",
+          method: 'DELETE',
         }
       );
       const data = await response.json();
       if (data.error) {
         toast.error(data.error);
       } else {
-        toast.success("Course deleted successfully!");
+        toast.success('Course deleted successfully!');
         // You might want to navigate to another page here or update the state to remove the deleted course
       }
     } catch (error) {
-      console.error("Error deleting course", error);
+      console.error('Error deleting course', error);
     }
   };
 
@@ -140,74 +139,74 @@ const UpdateCourse = () => {
   }
 
   return (
-    <div className='container w-50 shadow p-4 my-3'>
-      <h1 className='mb-2 d-flex justify-content-center'>Update Course</h1>
-      <div className='w-100 d-flex justify-content-end '>
-        <Tooltip title='Click to delete the course'>
-          <IconButton aria-label='delete' onClick={handleDeleteCourse}>
+    <div className="container w-50 shadow p-4 my-3">
+      <h1 className="mb-2 d-flex justify-content-center">Update Course</h1>
+      <div className="w-100 d-flex justify-content-end ">
+        <Tooltip title="Click to delete the course">
+          <IconButton aria-label="delete" onClick={handleDeleteCourse}>
             <DeleteIcon />
           </IconButton>
         </Tooltip>
       </div>
-      {error && <div className='alert alert-danger'>{error}</div>}
+      {error && <div className="alert alert-danger">{error}</div>}
       {course && (
         <form onSubmit={handleUpdate}>
-          <div className='mb-3'>
-            <label htmlFor='title' className='form-label'>
+          <div className="mb-3">
+            <label htmlFor="title" className="form-label">
               Course Title
             </label>
             <input
-              type='text'
-              className='form-control'
-              id='title'
-              placeholder='Enter course title'
-              name='title'
+              type="text"
+              className="form-control"
+              id="title"
+              placeholder="Enter course title"
+              name="title"
               onChange={(e) => handleChange(e)}
               value={course.title}
             />
           </div>
 
-          <div className='mb-3'>
-            <label htmlFor='description' className='form-label'>
+          <div className="mb-3">
+            <label htmlFor="description" className="form-label">
               Description
             </label>
             <textarea
-              className='form-control'
-              id='description'
-              placeholder='Enter course description'
-              name='description'
+              className="form-control"
+              id="description"
+              placeholder="Enter course description"
+              name="description"
               onChange={(e) => handleChange(e)}
               value={course.description}
             ></textarea>
           </div>
 
-          <div className='mb-3'>
-            <label htmlFor='category' className='form-label'>
+          <div className="mb-3">
+            <label htmlFor="category" className="form-label">
               Category
             </label>
             <select
-              className='form-select'
-              aria-label='Course category select'
-              name='category'
+              className="form-select"
+              aria-label="Course category select"
+              name="category"
               onChange={(e) => handleChange(e)}
               value={course.category}
             >
               <option>Select a course category</option>
-              <option value='business'>Business & Entrepreneurship</option>
-              <option value='computer-science'>Computer Science & IT</option>
-              <option value='health'>Health & Wellness</option>
-              <option value='arts'>Arts & Design</option>
-              <option value='education'>Education & Teaching</option>
-              <option value='engineering'>Engineering & Technology</option>
-              <option value='humanities'>Humanities & Social Sciences</option>
-              <option value='science'>Science & Mathematics</option>
-              <option value='languages'>Language Learning</option>
-              <option value='music'>Music & Performing Arts</option>
+              <option value="business">Business & Entrepreneurship</option>
+              <option value="computer-science">Computer Science & IT</option>
+              <option value="health">Health & Wellness</option>
+              <option value="arts">Arts & Design</option>
+              <option value="education">Education & Teaching</option>
+              <option value="engineering">Engineering & Technology</option>
+              <option value="humanities">Humanities & Social Sciences</option>
+              <option value="science">Science & Mathematics</option>
+              <option value="languages">Language Learning</option>
+              <option value="music">Music & Performing Arts</option>
             </select>
           </div>
 
-          <div className='mb-3 d-flex justify-content-start'>
-            <label htmlFor='image' className='form-label me-3'>
+          <div className="mb-3 d-flex justify-content-start">
+            <label htmlFor="image" className="form-label me-3">
               Image
             </label>
             <UploadWidget onUpload={setImage} />
@@ -215,22 +214,22 @@ const UpdateCourse = () => {
           {image && (
             <img
               src={image}
-              alt='Course Image'
-              className='img-fluid w-50 h-50'
-              style={{ maxWidth: "100px" }}
+              alt="Course Image"
+              className="img-fluid w-50 h-50"
+              style={{ maxWidth: '100px' }}
             />
           )}
 
-          <div className='mb-3'>
-            <label htmlFor='price' className='form-label'>
+          <div className="mb-3">
+            <label htmlFor="price" className="form-label">
               Price
             </label>
             <input
-              type='number'
-              className='form-control'
-              id='price'
-              placeholder='Enter course price'
-              name='price'
+              type="number"
+              className="form-control"
+              id="price"
+              placeholder="Enter course price"
+              name="price"
               onChange={(e) => handleChange(e)}
               value={course.price}
             />
@@ -239,7 +238,7 @@ const UpdateCourse = () => {
             course.content.map((content, index) => (
               <div
                 key={index}
-                className='d-flex justify-content-between align-items-center border p-2 my-2'
+                className="d-flex justify-content-between align-items-center border p-2 my-2"
               >
                 <p>{`Content ${index + 1}: ${content.type}`}</p>
                 {/* <button onClick={(e) => handleDeleteContent(content._id, e)}>
@@ -247,16 +246,16 @@ const UpdateCourse = () => {
                 </button> */}
                 <Delete
                   onClick={(e) => handleDeleteContent(content._id, e)}
-                  style={{ cursor: "pointer" }}
+                  style={{ cursor: 'pointer' }}
                 />
               </div>
             ))}
 
-          <div className='d-flex justify-content-center'>
+          <div className="d-flex justify-content-center">
             <button
-              type='submit'
-              className='btn'
-              style={{ borderColor: "#0455bf", color: "#0455bf" }}
+              type="submit"
+              className="btn"
+              style={{ borderColor: '#0455bf', color: '#0455bf' }}
               onClick={handleUpdate}
             >
               Update Course
