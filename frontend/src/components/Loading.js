0e7d9b0abe42588fc9282772316f553fcccef404
IT21209420 <it21209420@my.sliit.lk>
2025-05-18 02:21:35 +0530

    Refactor Loading component for consistent styling and add PropTypes validation

--- frontend/src/components/Loading.js
+++ frontend/src/components/Loading.js
@@ -1,15 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types"; // Import PropTypes
 
 //Loading functional component
 const Loading = ({ text }) => {
   return (
-    <div className='d-flex justify-content-center align-items-center'>
+    <div className="d-flex justify-content-center align-items-center">
       <div>
-        <div className='spinner-border text-primary' role='status'></div>
-        <span className='sr-only'>{text}...</span>
+        <div className="spinner-border text-primary" role="status"></div>
+        <span className="sr-only">{text}...</span>
       </div>
     </div>
   );
 };
+
+// Define prop types for the Loading component
+Loading.propTypes = {
+  text: PropTypes.string.isRequired, // Validate that text is a required string
+};
+
 // Export the Loading component as the default export
 export default Loading;
