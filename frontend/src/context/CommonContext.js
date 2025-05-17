3d310c21b2f2b4a4827ada25d04a6df4807648ef
IT21209420 <it21209420@my.sliit.lk>
2025-05-18 02:31:41 +0530

    Remove unused ToastContext usage from CommonContextProvider

--- frontend/src/context/CommonContext.js
+++ frontend/src/context/CommonContext.js
@@ -5,8 +5,6 @@ import ToastContext from "./ToastContext";
 const CommonContext = createContext();
 
 export const CommonContextProvider = ({ children }) => {
-  const { toast } = useContext(ToastContext);
-
   const [selectedCourseId, setSelectedCourseId] = useState(null);
   const [selectedCompanyId, setSelectedCompanyId] = useState(null);
   const [isEnrolled, setIsEnrolled] = useState(false);
