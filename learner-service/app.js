e443427521d64b7c0717c54ef474ea94a8a3377a
Ashen_IT21286278 <99505510+IT21286278@users.noreply.github.com>
2025-05-18 01:52:50 +0530

    Refactor app.js for consistent string quoting and improve error logging

--- learner-service/app.js
+++ learner-service/app.js
@@ -1,9 +1,9 @@
-import express from "express";
-import morgan from "morgan";
-import dotenv from "dotenv";
-import cors from "cors";
-import { connect } from "./config/db_con.js";
-import learnerRouter from "./routes/Learner.js";
+import express from 'express';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { connect } from './config/db_con.js';
+import learnerRouter from './routes/Learner.js';
 
 dotenv.config();
 
@@ -11,23 +11,25 @@ const app = express();
 
 //middlewares
 app.use(express.json()); //Send respones in json fomrat
-app.use(morgan("tiny")); //log requests
+app.use(morgan('tiny')); //log requests
 app.use(cors());
 
-app.use("/enrollment", learnerRouter);
+app.use('/enrollment', learnerRouter);
 
 //default route
-app.get("/", (req, res) => {
-  res.send("Welcome to the Learner Service");
+app.get('/', (req, res) => {
+  res.send('Welcome to the Learner Service');
 });
 
 //server configuration
 const PORT = process.env.PORT || 9004;
+
 app.listen(PORT, async () => {
   try {
     await connect();
-    console.log(`Sever is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
   } catch (error) {
-    console.log(err);
+    console.error('Failed to connect to the database:', error);
+    process.exit(1); // Exit the process with failure
   }
 });
